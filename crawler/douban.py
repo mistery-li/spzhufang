084fc1c2c74ca289074bfdb374b2720ddc347407
mistery-li <litujin1123@gmail.com>
2018-04-20 22:27:42 +0800

    crwaler init

---
+++ crawler/douban.py
@@ -0,0 +1,15 @@
+import requests
+
+headers = {
+    'User-Agent': '',
+    'Cookie': {},
+}
+
+class Douban:
+    def __init__(self):
+        pass
+        
+
+if __name__ == '__main__':
+    douban = Douban()
+    douban.get_request_html()
